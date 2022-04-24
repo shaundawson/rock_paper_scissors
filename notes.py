95008cd3ca8226a404e787bed823db8fd2d51f39
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-24 17:54:33 -0400

    generate random love score

--- notes.py
+++ notes.py
@@ -1,5 +1,12 @@
 # Randomisation and Python Lists
 import random
 
-random_integer = random.randint(1, 10)
-print(random_integer)
+# create random whole number
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+
+# create random floating point - between [0.0 to 1.0)
+# random_float = random.random()
+
+love_score = random.randint(1,100)
+print(f"Your love score is: {love_score}")
