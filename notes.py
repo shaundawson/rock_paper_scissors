2b23ab54c00198ad37442b2c76a784d8cd077fef
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-25 09:53:47 -0400

    nested lists notes

--- notes.py
+++ notes.py
@@ -48,8 +48,8 @@ import random
 
 # BANKER ROULETTE EXERCISE
 # Split string method
-names_string = input("Give me everybody\'s names, separated by a comma. ")
-names = names_string.split(", ")
+# names_string = input("Give me everybody\'s names, separated by a comma. ")
+# names = names_string.split(", ")
 # print(names_string)
 # print(names)
 
@@ -59,5 +59,22 @@ names = names_string.split(", ")
 # print(f"{payer} is going to buy the meal today!")
 
 # Solution Using random.choice
-payer = random.choice(names)
-print(f"{payer} is going to buy the meal today!")
+# payer = random.choice(names)
+# print(f"{payer} is going to buy the meal today!")
+
+
+# Nested Lists
+
+# states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+# num_of_states = len(states_of_america)
+#print(num_of_states)
+#print(states_of_america[num_of_states-1])
+
+# dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
+fruits = ["Strawberries", "Nectarines", "Spinach", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen)
